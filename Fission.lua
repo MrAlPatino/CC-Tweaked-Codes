d229b1a733adad9f413ddbd7c12e66d3e6466bf1
MrAlPatino <43314815+MrAlPatino@users.noreply.github.com>
2022-05-05 23:24:55 +0100

    Create Fission.lua

---
+++ Fission.lua
@@ -0,0 +1,25 @@
+local reactor = peripheral.wrap("fissionReactorLogicAdapter_1")
+local monitor = peripheral.wrap("top")
+
+monitor.clear()
+
+while true do 
+monitor.setCursorPos(1,1)
+monitor.write("Core Temp in K: "..reactor.getTemperature())
+monitor.setCursorPos(1,2)
+monitor.write("so far no meltdon")
+monitor.setCursorPos(1,3)
+monitor.write("reactor damage: "..reactor.getDamagePercent())
+if reactor.getTemperature() > 1200 then
+reactor.setBurnRate(0)
+monitor.setCursorPos(1,4)
+monitor.write("EMERGENCY OVERHEAT")
+end
+if reactor.getDamagePercent() > 10 then
+monitor.setCursorPos(1,5)
+monitor.write("WARNING: Core damage at: "..reactor.getDamagePercent().."% please do not operate")
+end
+sleep(0.2)
+monitor.clear()
+sleep(0.1)
+end
